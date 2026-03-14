18417f09307c91a740db2b97444553250240a99d
Mohd Safaat Ali <shafaat9820@gmail.com>
2026-03-14 08:19:21 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(port, "localhost", () => {
+  httpServer.listen(port, () => {
   log(`serving on port ${port}`);
 });
 })();
